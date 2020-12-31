18e8355e43b951b017f8309ec88dac66bc5a03c1
root <circleone78@gmail.com>
2020-12-31 20:20:07 +0900

    12.31

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import Auth from "routes/Auth";
-import Home from "routes/Home";
 import SearchForm from "components/SearchForm";
 import AuthForm from "components/AuthForm";
 
